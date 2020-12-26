2091866f5010725dd20b3272ed51c151ddcf3171
AkiraDemenech <guiakira.demenechmori@gmail.com>
2020-12-26 15:52:39 -0300

    mezclar listas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# barajar
+Embora consiga embaralhar listas quaisquer também
+
+ # un barajador de líneas de código 
+
+Inspirado na ideia de mesclar alguns _scripts_ coesos de maneira que se mantenham sintaticamente corretos mas que baguncem sua funcionalidade, diferente do [sorteio das palavras](https://akirademenech.github.io/index.html#Dasur) do [github.com/akirademenech/L-oeuvre_dada](https://github.com/AkiraDemenech/L-oeuvre_dada/blob/master/poema.java.py.txt)/
---
+++ barajar/barajar.py
@@ -0,0 +1,78 @@
+import random
+
+recuo = {
+	'\t':	4,
+    ' ': 1
+}
+
+def contarecuo (p, r = 0, valores = recuo):
+	
+	try:
+		if type(p) != str:
+			return p.recuo
+
+		for c in p:
+			r += valores[c]
+	except AttributeError:
+		return contarecuo(p[0],r,valores)
+	except KeyError:
+		pass
+	return r
+
+	
+
+class linha:
+
+	def seguinte (self, next = None):
+		
+		try:
+			if next == None:	
+				return self.prox
+		#	else:
+			self.indentar = next.recuo - self.recuo
+		except AttributeError:
+			pass
+		else:
+			pass
+
+		self.prox = next
+
+	def linha (self, ln = None, ind = recuo):
+
+		if ln == None:
+			try:
+				return self.ln
+			except AttributeError:
+				pass
+
+		self.ln = ln
+		self.recuo = contarecuo(ln, valores = ind)
+
+	def __init__ (self, cont = None, prox = None, indentation = recuo):
+		self.seguinte(prox)
+		self.linha(cont, indentation)
+	#	self.cont = cont
+
+def embaralhar (*programas):
+	programas = list(programas) # lista de listas (estas últimas que podem ser encadeadas) que serão utilizadas
+	embaralhado = [] # programa de resultado
+
+	while len(programas) > 0:
+		s = random.randint(0,len(programas)-1)
+		s = programas.pop(s)
+		try:
+			if type(s) == list:
+				embaralhado.append(s.pop(0))
+			else:
+				embaralhado.append(s)
+				s = s.prox
+		except AttributeError:
+			pass # caso a linha não tenha uma próxima, 
+		else:	 # ela não é mantida no rol de sorteio
+			if len(s):
+				programas.append(s)
+	return embaralhado
+
+
+
+print(embaralhar([0,2,4,6,8],[1,[3,5,7],9,11,13,15,16],-1))
