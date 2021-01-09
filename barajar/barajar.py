895ac1ca7e6e4ba365b9497d1d4fcb8dfbf575b4
AkiraDemenech <guiakira.demenechmori@gmail.com>
2021-01-08 23:25:34 -0300

    As diferenças agora têm lado, parcialmente
    Algumas coisas ainda são estranhas, os dias estão bastante, mas menos exceções acontecem
    senão passe

--- barajar/barajar.py
+++ barajar/barajar.py
@@ -17,11 +17,40 @@ def contarecuo (p, r = 0, valores = recuo):
 		for c in p:
 			r += valores[c]
 	except AttributeError:
-		return contarecuo(p[0],r,valores)
+		if p != None:
+			return contarecuo(p[0],r,valores)
 	except KeyError:
 		pass
 	return r
 
+def blocos (prog, novo = False):
+	if novo or type(prog) == str:
+		prog = linhas(prog)
+	novo = True
+	coment = '#','"""',"'''"
+	ind = b = c = 0
+	while c < len(prog):
+		for i in coment:
+			if prog[c].find(i) == 0 or len(prog[c]) == 0:
+				prog[c].recuo = ind
+				break
+		ind = contarecuo(prog[c])
+		if '#@bloco;' in prog[c]:
+			prog[c] = [prog[c]]
+			b = c
+			c += 1
+			novo = False
+		elif novo:
+			c += 1
+		else:
+			if '#@bloco_acabou;' in prog[c]:
+				novo = True
+			prog[b].append(prog.pop(c))
+	#	c += novo
+	return prog
+			
+			
+			
 	
 def linhas (prog,prox=None,id=None,ind=recuo):
 	if type(prog) == str:
@@ -45,14 +74,16 @@ class linha:
 		try:
 			if next == None:	
 				return self.prox
-		#	else:
-			self.indentar = next.recuo - self.recuo
 		except AttributeError:
-			pass
-		else:
 			pass
 
 		self.prox = next
+
+	def indentar (self, r = 0):
+		try:
+			return self.prox.recuo - self.recuo
+		except AttributeError:
+			return r
 
 	def linha (self, ln = None, ind = recuo):
 
@@ -82,6 +113,24 @@ class linha:
 				self.recuo = ind
 			return
 		self.ln = ln[c:]
+	
+	def index (self,*item):
+		return self.ln.index(*item)
+	def find (self, *item):
+		try:
+			return self.index(*item)
+		#	return self.ln.find(*item)
+		except AttributeError:
+			return -2
+		except:
+			return -1
+		
+
+	def __contains__ (self, item):
+		try:
+			return self.ln.__contains__(item)
+		except Exception:
+			return 
 
 	def __str__ (self):
 		return self.cru.__str__()
@@ -99,20 +148,42 @@ class linha:
 	def __init__ (self, valor = None, prox = None, indentation = recuo, id = None):
 		if prox == None and valor.__class__ == self.__class__:
 			prox = valor.prox
+		self.id = id
 		self.seguinte(prox)
 		self.linha(valor, indentation)
-		self.id = id
+		
+
 	def __id__ (self,id=None):
 		if id == None:
 			return self.id
 		self.id = id
 
+	def __gt__ (self,outro):
+		return not self.__le__(outro)
+	def __lt__ (self,outro):
+		return not self.__ge__(outro)
 	def __eq__ (self,outro):
 		return not self.__ne__(outro)
+
 	def __ne__ (self, outro):		
 		if self.__class__ == outro.__class__:
 			return self.recuo != outro.recuo or self.ln != outro.ln
 		return self.cru != outro and self.ln != outro
+	def __ge__ (self, outro):
+		try:
+			return self.recuo >= outro.recuo or self.indentar() >= outro.indentar() 
+		except AttributeError:	
+			return self.__str__() > str(outro) or self.__eq__(outro)
+	def __le__ (self, outro):	
+		try: 
+			return self.recuo <= outro.recuo or self.indentar() <= outro.indentar()
+		except AttributeError:
+			return self.__str__() < str(outro) or self.__eq__(outro)
+	def __len__ (self):
+		try:
+			return self.ln.__len__()
+		except AttributeError:
+			return -1
 
 def embaralhar (*programas):
 	programas = list(programas) # lista de listas (estas últimas que podem ser encadeadas) que serão utilizadas
